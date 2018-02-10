0bf0a6caf4745e2771f3fc87aa84d9448bc80d6b
Tjen Wellens <(none)>
2018-02-10 17:15:04 +0100

    given_11_expects_1

--- day1/src/test/java/com/example/day1/Day1ApplicationTests.java
+++ day1/src/test/java/com/example/day1/Day1ApplicationTests.java
@@ -5,6 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Day1ApplicationTests {
@@ -13,4 +19,15 @@ public class Day1ApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void given_11_expects_1() throws Exception {
+		int result = captcha("11");
+		assertThat(result, is(equalTo(1)));
+	}
+
+	private int captcha(String s) {
+        final String[] chars = s.split("");
+        final int[] digits = Arrays.asList(chars).stream().mapToInt(Integer::parseInt).toArray();
+        return digits[0];
+	}
 }
