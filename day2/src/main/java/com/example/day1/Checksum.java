38f7ff0cb3bfd68f8b82dd564722ac5f578238b6
Tjen Wellens <(none)>
2018-02-10 18:39:09 +0100

    refactor

---
+++ day2/src/main/java/com/example/day1/Checksum.java
@@ -0,0 +1,7 @@
+package com.example.day1;
+
+public class Checksum {
+    public int calculate(String s) {
+        return 0;
+    }
+}
--- day2/src/test/java/com/example/day1/Day2ApplicationTests.java
+++ day2/src/test/java/com/example/day1/Day2ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.day1;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
@@ -11,19 +12,26 @@ import static org.junit.Assert.assertThat;
 @RunWith(BlockJUnit4ClassRunner.class)
 public class Day2ApplicationTests {
 
+    private Checksum checksum;
+
+    @Before
+    public void setUp() throws Exception {
+        checksum = new Checksum();
+    }
+
     @Test
     public void row_5_1_9_5_returns_8() throws Exception {
-        assertThat(checksum("5 1 9 5"), is(equalTo(8)));
+        assertThat(checksum.calculate("5 1 9 5"), is(equalTo(8)));
     }
 
     @Test
     public void row_7_5_3_returns_4() throws Exception {
-        assertThat(checksum("7 5 3"), is(equalTo(4)));
+        assertThat(checksum.calculate("7 5 3"), is(equalTo(4)));
     }
 
     @Test
     public void row_2_4_6_8_returns_6() throws Exception {
-        assertThat(checksum("2 4 6 8"), is(equalTo(6)));
+        assertThat(checksum.calculate("2 4 6 8"), is(equalTo(6)));
     }
 
     @Test
@@ -35,10 +43,6 @@ public class Day2ApplicationTests {
                 + "\n"
                 + "2 4 6 8"
                 ;
-        assertThat(checksum(sheet), is(equalTo(18)));
-    }
-
-    private int checksum(String s) {
-        return 0;
+        assertThat(checksum.calculate(sheet), is(equalTo(18)));
     }
 }
