a9e9b4b0c4f281542a5433fe850825288ce8e941
Tjen Wellens <(none)>
2018-02-10 17:53:34 +0100

    solution 1 star

---
+++ day1/src/main/java/com/example/day1/AppRunner.java
@@ -0,0 +1,25 @@
+package com.example.day1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Component
+public class AppRunner implements CommandLineRunner {
+    private static final Logger log = LoggerFactory.getLogger(AppRunner.class);
+    @Override
+    public void run(String... strings) throws Exception {
+        final URI path = new ClassPathResource("input.txt").getURI();
+        log.debug("URI: " + path);
+        String content = new String(Files.readAllBytes(Paths.get(path)));
+        log.debug("CONTENT: " + content);
+        final int solution = new CaptchaSolver().solve(content);
+        log.info("solution: " + solution);
+    }
+}
