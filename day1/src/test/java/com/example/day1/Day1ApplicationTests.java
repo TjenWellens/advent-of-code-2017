21b5e67e450828c530aeeab229988842e927b36d
Tjen Wellens <(none)>
2018-02-10 17:27:12 +0100

    given_1111_expects_4

--- day1/src/test/java/com/example/day1/Day1ApplicationTests.java
+++ day1/src/test/java/com/example/day1/Day1ApplicationTests.java
@@ -12,7 +12,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest
 public class Day1ApplicationTests {
 
 	@Test
@@ -20,14 +20,38 @@ public class Day1ApplicationTests {
 	}
 
 	@Test
-	public void given_11_expects_1() throws Exception {
-		int result = captcha("11");
+	public void given_111_expects_1() throws Exception {
+		int result = captcha("112");
 		assertThat(result, is(equalTo(1)));
 	}
 
-	private int captcha(String s) {
-        final String[] chars = s.split("");
-        final int[] digits = Arrays.asList(chars).stream().mapToInt(Integer::parseInt).toArray();
-        return digits[0];
+	@Test
+	public void given_1122_expects_3() throws Exception {
+		int result = captcha("1122");
+		assertThat(result, is(equalTo(3)));
 	}
+
+	@Test
+	public void given_1111_expects_4() throws Exception {
+		int result = captcha("1111");
+		assertThat(result, is(equalTo(4)));
+	}
+
+	private int captcha(String s) {
+        final String[] chars = (s + s.substring(0,1)).split("");
+        final int[] digits = Arrays.asList(chars).stream().mapToInt(Integer::parseInt).toArray();
+        return addIfSameDigitAsBefore(digits, 1);
+	}
+
+	private int addIfSameDigitAsBefore(int[] digits, int index) {
+	    if(index >= digits.length)
+	        return 0;
+
+	    int previousDigit = digits[index - 1];
+	    int currentDigit = digits[index];
+	    if(previousDigit == currentDigit)
+	        return previousDigit + addIfSameDigitAsBefore(digits, index + 1);
+	    else
+	        return addIfSameDigitAsBefore(digits, index + 1);
+    }
 }
