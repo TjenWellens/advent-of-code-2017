5ea1c6ec5c0141fb756470b61c10dd8c2ef6fc9a
Tjen Wellens <(none)>
2018-02-10 18:47:16 +0100

    part 1 done

--- day2/src/main/java/com/example/day1/Checksum.java
+++ day2/src/main/java/com/example/day1/Checksum.java
@@ -1,7 +1,21 @@
 package com.example.day1;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Checksum {
-    public int calculate(String s) {
-        return 0;
+    public int calculate(String sheet) {
+        if(sheet.contains("\n")) {
+            return Arrays.stream(sheet.split("\n"))
+                    .mapToInt(this::calculate)
+                    .sum();
+        }
+
+        String row = sheet;
+        return getIntStream(row).max().getAsInt() - getIntStream(row).min().getAsInt();
+    }
+
+    private IntStream getIntStream(String row) {
+        return Arrays.stream(row.split(" ")).mapToInt(Integer::parseInt);
     }
 }
