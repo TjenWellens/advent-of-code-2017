2eda7e44d15feca7d8caabf7242e84cad5dc403d
Tjen Wellens <(none)>
2018-02-14 17:04:34 +0100

    day 3 init

---
+++ day3/src/test/java/com/example/day1/Day3ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.day1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class Day3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
