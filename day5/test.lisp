deab780aaf7016cacd1f57abce036a0ebbb4fc50
Tjen Wellens <(none)>
2018-02-27 10:29:15 +0100

    refactor

--- day5/test.lisp
+++ day5/test.lisp
@@ -2,22 +2,30 @@
 
 (load "code.lisp")
 
-(defparameter +steps+ '(
-                  (0 (0 3 0 1 -3))
-                  (0 (1 3 0 1 -3))
-                  (1 (2 3 0 1 -3))
-                  (4 (2 4 0 1 -3))
-                  (1 (2 4 0 1 -2))
-                (nil (2 5 0 1 -2))
-                  ))
+(defun mk-offs (index offsets)
+  (list index (copy-list offsets)))
+
+(defparameter +steps+ (list
+                       (mk-offs 0   '(0 3 0 1 -3))
+                       (mk-offs 0   '(1 3 0 1 -3))
+                       (mk-offs 1   '(2 3 0 1 -3))
+                       (mk-offs 4   '(2 4 0 1 -3))
+                       (mk-offs 1   '(2 4 0 1 -2))
+                       (mk-offs nil '(2 5 0 1 -2))
+                       ))
+
+(defun get-offs (index)
+  (destructuring-bind
+   (i offs) (nth index +steps+)
+   (mk-offs i offs)))
 
 (deftest test-next-offset ()
   (check
-   (equal (apply #'next-offset (nth 0 +steps+)) (nth 1 +steps+))
-   (equal (apply #'next-offset (nth 1 +steps+)) (nth 2 +steps+))
-   (equal (apply #'next-offset (nth 2 +steps+)) (nth 3 +steps+))
-   (equal (apply #'next-offset (nth 3 +steps+)) (nth 4 +steps+))
-   (equal (apply #'next-offset (nth 4 +steps+)) (nth 5 +steps+))))
+   (equal (apply #'next-offset (get-offs 0)) (get-offs 1))
+   (equal (apply #'next-offset (get-offs 1)) (get-offs 2))
+   (equal (apply #'next-offset (get-offs 2)) (get-offs 3))
+   (equal (apply #'next-offset (get-offs 3)) (get-offs 4))
+   (equal (apply #'next-offset (get-offs 4)) (get-offs 5))))
 
 (deftest test-count-steps ()
   (check
